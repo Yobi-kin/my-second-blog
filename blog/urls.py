e6ed1e6778d5e75ffd5d8ed18bdd2e68a1b458a9
Nick <roddenick@gmail.com>
2024-01-26 10:49:40 +0100

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("post/<int:pk>/", views.post_detail, name="post_detail"),
     path("post/new/", views.post_new, name="post_new"),
-    path("admin/", views.admin, name="admin"),
+    path("admin/auth/", views.admin, name="admin"),
 ]
